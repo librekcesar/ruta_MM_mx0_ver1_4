e177023422d4b662ce11650788ad12cb96a14dd5
librekcesar <140665209+librekcesar@users.noreply.github.com>
2025-07-15 16:49:54 -0600

    Add files via upload

---
+++ estaciones_mx0_ver1_ver2_ver4_copia.js
@@ -0,0 +1,10 @@
+var estaciones = {
+"type": "FeatureCollection",
+"name": "estaciones_mx0_ver1_ver2_ver4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "19.64220097106514, -99.14250999381147", "description": "MX0", "t_estacion": "Origen" }, "geometry": { "type": "Point", "coordinates": [ -99.1422607, 19.6422227, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "C Jorullo 42, Aguacatal, 91133 Xalapa-Enríquez, Ver., México", "description": "VER4", "t_estacion": "Destino" }, "geometry": { "type": "Point", "coordinates": [ -96.9228372, 19.5430802, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Estambul 36, Hacienda Los Portales Secc Sur, 91726 Nuevo Veracruz, Ver., México", "description": "VER1", "t_estacion": "Origen" }, "geometry": { "type": "Point", "coordinates": [ -96.1921431, 19.1821128, 0.0 ] } }
+]
+}
---
+++ paradas_mx0_ver1_ver2_ver4_copia.js
@@ -0,0 +1,12 @@
+var seguras = {
+"type": "FeatureCollection",
+"name": "paradas_mx0_ver1_ver2_ver4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Pemex", "description": "Carretera Fed. Xalapa-Veracruz km 37.5, Palo Gacho, 91633 Villa Emiliano Zapata, Ver.", "layer": "PEMEX_MX0_VER1_4", "direccion": "Carretera Fed. Xalapa-Veracruz km 37.5, Palo Gacho, 91633 Villa Emiliano Zapata, Ver." }, "geometry": { "type": "Point", "coordinates": [ -96.6280882, 19.393073, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Pemex", "description": "Carrr. Fed. Mex.-Xalapa Km. 323, Centro, 91300 Banderilla, Ver.", "layer": "PEMEX_MX0_VER1_4", "direccion": "Carrr. Fed. Mex.-Xalapa Km. 323, Centro, 91300 Banderilla, Ver." }, "geometry": { "type": "Point", "coordinates": [ -96.9417554, 19.5922419, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "ONE Gasolinería Axapusco", "description": "Carretera México-Tulancingo KM 39+700, 85, 55955 Méx.", "layer": "PEMEX_MX0_VER1_4", "direccion": "Carretera México-Tulancingo KM 39+700, 85, 55955 Méx." }, "geometry": { "type": "Point", "coordinates": [ -98.7372028, 19.7804487, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Pemex", "description": "Av 31 Pte 521, Insurgentes Chula Vista, 72420 Heroica Puebla de Zaragoza, Pue.", "layer": "PEMEX_MX0_VER1_4", "direccion": "Av 31 Pte 521, Insurgentes Chula Vista, 72420 Heroica Puebla de Zaragoza, Pue." }, "geometry": { "type": "Point", "coordinates": [ -97.5012787, 19.507058, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Gasolinera Pemex SG Gasolineros", "description": "México 150D, 72680 San Francisco Ocotlán, Pue.", "layer": "PEMEX_MX0_VER1_4", "direccion": "México 150D, 72680 San Francisco Ocotlán, Pue." }, "geometry": { "type": "Point", "coordinates": [ -98.2776772, 19.1405144, 0.0 ] } }
+]
+}
